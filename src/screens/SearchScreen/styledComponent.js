ffe2f36949adef66960541bddad3cf253df9b103
ashuvssut <ashu.khanduala@gmail.com>
2021-12-18 23:52:38 +0530

    map profile card done

--- src/screens/SearchScreen/styledComponent.js
+++ src/screens/SearchScreen/styledComponent.js
@@ -1,38 +1,75 @@
-import styled from 'styled-components';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import styled from "styled-components";
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export const FilterView = styled.Pressable`
-position: absolute;
-bottom:${hp('5')}px;
-left:${wp('5')}px;
-width:${wp('30')}px;
-height: ${hp('6')}px;
-border-radius:${hp('1')}px;
-background-color: black;
-justify-content: space-around;
-align-items: center;
-flex-direction: row;
+	position: absolute;
+	bottom: ${hp("5")}px;
+	left: ${wp("5")}px;
+	width: ${wp("30")}px;
+	height: ${hp("6")}px;
+	border-radius: ${hp("1")}px;
+	background-color: black;
+	justify-content: space-around;
+	align-items: center;
+	flex-direction: row;
 `;
 
 export const FilterText = styled.Text`
-color:white;
-font-size:${hp('2.4')}px;
+	color: white;
+	font-size: ${hp("2.4")}px;
 `;
 
 export const MapCard = styled.View`
-width:${wp('60')}px;
-border-radius: ${hp('1')}px;
-height:${hp('20')}px;
-background-color: black;
-justify-content: center;
-align-items: center;
+	width: ${wp("60")}px;
+	border-radius: ${hp("1")}px;
+	background-color: black;
+	justify-content: flex-start;
+	align-items: flex-start;
+	padding: ${hp("2")}px ${hp("2")}px ${hp("2")}px ${hp("2")}px;
 `;
 
-export const Title= styled.Text`
-color:white;
-font-size:${hp('2.4')}px;
+export const Profile = styled.View`
+	flex-direction: row;
 `;
-export const Description= styled.Text`
-color:white;
-font-size:${hp('1.8')}px;
+
+export const Avatar = styled.View`
+	width: ${wp("15")}px;
+	height: ${wp("15")}px;
+	margin: ${wp("2")}px;
+	border-radius: 200px;
+	background-color: #0f0;
+`;
+
+export const Title = styled.Text`
+	color: white;
+	font-size: ${hp("2.4")}px;
+	margin-bottom: ${hp("1")}px;
+`;
+export const Description = styled.Text`
+	color: white;
+	font-size: ${hp("1.3")}px;
+`;
+export const Capitalize = styled.Text`
+	text-transform: capitalize;
+`;
+export const Bold = styled.Text`
+	font-weight: bold;
+`;
+export const Italic = styled.Text`
+	font-style: italic;
+`;
+
+export const RequestButton = styled.TouchableOpacity`
+	width: 100%;
+	height: ${hp("5")}px;
+	background-color: #f88386;
+	align-items: center;
+	justify-content: space-evenly;
+	border-radius: 100px;
+	margin-top: 10px;
+	elevation: 5;
+	flex-direction: row;
 `;
