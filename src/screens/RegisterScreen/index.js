6871eca9ae0866a4151b994b501383f72319a266
ashuvssut <ashu.khanduala@gmail.com>
2021-12-22 04:14:35 +0530

    keyboard avoiding view component

--- src/screens/RegisterScreen/index.js
+++ src/screens/RegisterScreen/index.js
@@ -12,7 +12,6 @@ import {
 	widthPercentageToDP as wp,
 	heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import { Screen } from "../../components/Screen";
 import {
 	SignupButton,
 	LoginText,
@@ -26,96 +25,95 @@ import Errors from "../../components/Errors";
 import DisabledButton from "../../components/DisabledButton";
 import { TopWaveWB } from "svg";
 import perfectSize from "../../utils/pixelPerfect";
+import { KeyboardUsingScreen } from "../../components/KeyboardUsingScreen";
 
 export default function index({ navigation }) {
 	return (
-		<Screen>
-			<Pressable style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
-				<TopWaveWB width={perfectSize(411)} height={perfectSize(327.33)} />
-				<Text style={styles.heading}>REGISTER</Text>
+		<KeyboardUsingScreen>
+			<TopWaveWB width={perfectSize(411)} height={perfectSize(327.33)} />
+			<Text style={styles.heading}>REGISTER</Text>
 
-				<BottomView>
-					<Formik
-						validationSchema={schema}
-						initialValues={{
-							name: "",
-							email: "",
-							age: "",
-							phoneNumber: "",
-						}}
-						onSubmit={values => navigation.navigate("Register2", { values })}
-						validateOnMount
-					>
-						{({
-							handleChange,
-							handleBlur,
-							handleSubmit,
-							values,
-							errors,
-							isValid,
-							touched,
-						}) => (
-							<>
-								<StyledInput
-									value={values.name}
-									onChangeText={handleChange("name")}
-									placeholder="Enter Name"
-									autoCorrect={false}
-									onBlur={handleBlur("name")}
-									placeholder="Name"
-								/>
-								{values.name.length !== 0 && errors.name && touched.name && (
-									<Errors texts={errors.name} />
-								)}
-								<StyledInput
-									value={values.email}
-									onChangeText={handleChange("email")}
-									placeholder="Enter Email Address"
-									autoCorrect={false}
-									onBlur={handleBlur("email")}
-									placeholder="Email"
-								/>
-								{values.email.length !== 0 && errors.email && touched.email && (
-									<Errors texts={errors.email} />
-								)}
-								<StyledInput
-									value={values.age}
-									onChangeText={handleChange("age")}
-									autoCorrect={false}
-									onBlur={handleBlur("age")}
-									placeholder="Age"
-								/>
-								{values.age < 18 && errors.age && touched.age && (
-									<Errors texts={errors.age} />
-								)}
-								<StyledInput
-									value={values.phoneNumber}
-									onChangeText={handleChange("phoneNumber")}
-									autoCorrect={false}
-									onBlur={handleBlur("phoneNumber")}
-									placeholder="Mobile (10-Digits)"
-								/>
-								{values.phoneNumber.length !== 0 &&
-									errors.phoneNumber &&
-									touched.phoneNumber && <Errors texts={errors.phoneNumber} />}
-								{isValid ? (
-									<LoginButton onPress={handleSubmit}>
-										<LoginText>NEXT (1/2)</LoginText>
-									</LoginButton>
-								) : (
-									<DisabledButton>
-										<LoginText>NEXT (1/2)</LoginText>
-									</DisabledButton>
-								)}
-							</>
-						)}
-					</Formik>
-					<SignupButton onPress={() => navigation.navigate("Login")}>
-						<LoginText>BACK</LoginText>
-					</SignupButton>
-				</BottomView>
-			</Pressable>
-		</Screen>
+			<BottomView>
+				<Formik
+					validationSchema={schema}
+					initialValues={{
+						name: "",
+						email: "",
+						age: "",
+						phoneNumber: "",
+					}}
+					onSubmit={values => navigation.navigate("Register2", { values })}
+					validateOnMount
+				>
+					{({
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						values,
+						errors,
+						isValid,
+						touched,
+					}) => (
+						<>
+							<StyledInput
+								value={values.name}
+								onChangeText={handleChange("name")}
+								placeholder="Enter Name"
+								autoCorrect={false}
+								onBlur={handleBlur("name")}
+								placeholder="Name"
+							/>
+							{values.name.length !== 0 && errors.name && touched.name && (
+								<Errors texts={errors.name} />
+							)}
+							<StyledInput
+								value={values.email}
+								onChangeText={handleChange("email")}
+								placeholder="Enter Email Address"
+								autoCorrect={false}
+								onBlur={handleBlur("email")}
+								placeholder="Email"
+							/>
+							{values.email.length !== 0 && errors.email && touched.email && (
+								<Errors texts={errors.email} />
+							)}
+							<StyledInput
+								value={values.age}
+								onChangeText={handleChange("age")}
+								autoCorrect={false}
+								onBlur={handleBlur("age")}
+								placeholder="Age"
+							/>
+							{values.age < 18 && errors.age && touched.age && (
+								<Errors texts={errors.age} />
+							)}
+							<StyledInput
+								value={values.phoneNumber}
+								onChangeText={handleChange("phoneNumber")}
+								autoCorrect={false}
+								onBlur={handleBlur("phoneNumber")}
+								placeholder="Mobile (10-Digits)"
+							/>
+							{values.phoneNumber.length !== 0 &&
+								errors.phoneNumber &&
+								touched.phoneNumber && <Errors texts={errors.phoneNumber} />}
+							{isValid ? (
+								<LoginButton onPress={handleSubmit}>
+									<LoginText>NEXT (1/2)</LoginText>
+								</LoginButton>
+							) : (
+								<DisabledButton>
+									<LoginText>NEXT (1/2)</LoginText>
+								</DisabledButton>
+							)}
+						</>
+					)}
+				</Formik>
+				<SignupButton onPress={() => navigation.navigate("Login")}>
+					<LoginText>BACK</LoginText>
+				</SignupButton>
+			</BottomView>
+		</KeyboardUsingScreen>
 	);
 }
 
